1b9bc3ce5c3836c78f13da8e3ef30a76e5643483
jinsil.hwang <jinsil.kelly.hwang@gmail.com>
2020-07-19 00:00:06 +0100

    fix: updated sample.js

--- src/sample.js
+++ src/sample.js
@@ -1,3 +1,16 @@
-const foo = 'hello';
+// https://leetcode.com/problems/plus-one/
 
-console.log(foo)
+const recursion = (target, arr) => {
+  const sum = target.pop() + 1;
+  if (sum === 10) {
+    arr.unshift(0);
+    return !target.length ? [1, ...arr] : recursion(target, arr);
+  }
+    arr.unshift(sum);
+    return [...target, ...arr];
+
+}
+
+const plusOne = (digits) => {
+  return recursion([...digits], []);
+};
