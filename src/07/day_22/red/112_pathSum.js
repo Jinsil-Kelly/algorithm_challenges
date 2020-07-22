fadb362e2be760ba65ed879d9b515a49258565c0
jinsil.hwang <jinsil.kelly.hwang@gmail.com>
2020-07-23 00:08:27 +0100

    chore: fixed mistakes

--- src/07/day_22/red/112_pathSum.js
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/path-sum/
-
-const hasPathSum = (root, sum) => {
-  if (!root) return false;
-  const cur = sum - root.val;
-  if (!root.left && !root.right && cur === 0) return true;
-  return hasPathSum(root.left, cur) || hasPathSum(root.right, cur);
-};
--- src/07/day_22/red/217_containsDuplicate.js
+++
@@ -1,3 +0,0 @@
-// https://leetcode.com/problems/contains-duplicate/
-
-const containsDuplicate = (nums) => new Set(nums).size !== nums.length;
