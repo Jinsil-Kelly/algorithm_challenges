7bc90db77cb5c8bb88161a46b5307fbc5ad9f118
jinsil.hwang <jinsil.kelly.hwang@gmail.com>
2020-07-27 23:42:15 +0100

    fix: removed 27_red

--- src/07/day_27/red/111_minimumDepthOfBinaryTree.js
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/minimum-depth-of-binary-tree/
-
-const minDepth = function (root) {
-  if (!root) return 0; // add 0
-  if (!root.left) return 1 + minDepth(root.right); // [1,2] => 2 when right skew
-  if (!root.right) return 1 + minDepth(root.left); // when left skew
-  return 1 + Math.min(minDepth(root.left), minDepth(root.right));
-};
