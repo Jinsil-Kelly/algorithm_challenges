f1d43c597db593497916085bd97d71700d071cbd
Yohan Choi <ychoi63@myseneca.ca>
2020-07-28 06:57:30 -0400

    fix: correct invalid index errors & minor typos

--- src/07/day_27/green/ThreeNumberSum.js
+++ src/07/day_27/green/ThreeNumberSum.js
@@ -4,24 +4,25 @@
 
 // O(n^2) | O(n) space
 exports.threeNumberSum = (nums, target) => {
-    Array.sort((a,b) => a - b);
-    const triplets = [];
+  nums.sort((a, b) => a - b);
+  const triplets = [];
 
-    for (let i = 0; i < nums.length - 2; i++) {
-        let lett = i + 1;
-        let right = nums.length - 1;
-        while (letf < right) {
-            const sum = nums[i] + nums[left] + nums[right];
-
-            if (sum === target) {
-                triplets.push([nums[i], nums[left], nums[right]]);
-                left++;
-                right++;
-            } else if (sum < target) {
-                left++;
-                right--;
-            }
-        }
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum === target) {
+        triplets.push([nums[i], nums[left], nums[right]]);
+        left++; // eslint-disable-line no-plusplus
+        right--; // eslint-disable-line no-plusplus
+      } else if (sum < target) {
+        left++; // eslint-disable-line no-plusplus
+      } else if (sum > target) {
+        right--; // eslint-disable-line no-plusplus
+      }
     }
-    return triplets;
-}
+  }
+  return triplets;
+};
