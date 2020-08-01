9c3caad734653f69be188a075af2a62456c21e46
kmin-jeong <desk535353@gmail.com>
2020-08-01 15:18:55 +0900

    fix: solve 21

--- src/08/day_1/green/merge-two-sorted-lists.js
+++ src/08/day_1/green/merge-two-sorted-lists.js
@@ -14,13 +14,3 @@ function mergeTwoLists(l1, l2) {
 }
 
 mergeTwoLists();
-
-// /*const mergeTwoLists = function(l1, l2) {
-//   if (!l1 || !l2)
-//   return l1 || l2;
-//   const linkThem = (smaller, greater) => {
-//        mergeTwoLists(smaller.next, greater);
-//       return smaller;
-//   };
-//   return l1.val < l2.val ? linkThem(l1,l2) : linkThem(l2,l1);
-// };
