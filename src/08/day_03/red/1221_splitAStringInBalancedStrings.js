0a6cec8dc51b9fa6595753c0134cc9661a328247
jinsil.hwang <jinsil.kelly.hwang@gmail.com>
2020-08-04 00:12:25 +0100

    feat: added the other solution on 1221

--- src/08/day_03/red/1221_splitAStringInBalancedStrings.js
+++ src/08/day_03/red/1221_splitAStringInBalancedStrings.js
@@ -1,6 +1,26 @@
 // https://leetcode.com/problems/split-a-string-in-balanced-strings/
 
-const balancedStringSplit = (s) => {
+const balancedStringSplitSolution1 = (s) => {
+  const L = 'L';
+  let balance = 0;
+  let count = 0;
+
+  for (let i = 0; i < s.length; i += 1) {
+    const char = s[i];
+
+    if (char === L) {
+      balance += 1;
+    } else {
+      balance -= 1;
+    }
+
+    if (!balance) count += 1;
+  }
+
+  return count;
+};
+
+const balancedStringSplitSolution2 = (s) => {
   const L = 'L';
   let arrR = [];
   let arrL = [];
