8f50afe17103d16a10453eec1e91afb35a6802c8
kmin-jeong <desk535353@gmail.com>
2020-08-04 12:49:27 +0900

    fix: solve 1202

--- src/08/day_04/green/1202.smallestStringWithSwaps.js
+++ src/08/day_04/green/1202.smallestStringWithSwaps.js
@@ -1,40 +1,42 @@
 //https://leetcode.com/problems/smallest-string-with-swaps/
 
-const smallestStringWithSwaps = function(s, pairs) {
-    let set = Array(s.length).fill(-1)
-    function union(a, b) {//a,b
-      let r1 = find(a)
-      let r2 = find(b)
-      if (r1 !== r2) {
-        set[r2] = r1
-      }
-    }
-    function find(a) {
-      if (set[a] < 0) {
-        return a
-      } else {
-        return (set[a] = find(set[a]))
-      }
-    }
-    for (let p of pairs) {
-      union(p[0], p[1])
-    }
-    let gs = []//making group
-    for (let x = 0; x < s.length; x++) {
-      gs[x] = []
-    }
-    for (let x = 0; x < s.length; x++) {
-      gs[find(x)].push(x)
-    }// find ==> push
-    let sArr = s.split('')
-    for (let x = 0; x < s.length; x++) {
-      if (gs[x].length > 1) {
-        let chars = gs[x].map(idx => s[idx])
-        chars.sort()
-        for (let k = 0; k < gs[x].length; k++) {
-          sArr[gs[x][k]] = chars[k]
+const smallestStringWithSwaps = function(s,pairs){
+    let set  = Array(s.length).fill(-1)
+    function union(a,b){
+        let r1 = find(a)
+        let r2 = find(b)
+        if(r1 !== r2){
+            set[r2] = r1
         }
-      }
+    }
+    function find(a){
+        if(set[a] < 0){
+            return a
+        }
+        else{
+            return(set[a] = find(set[a]))
+        }
+    }
+    for(let p of pairs){
+        union(p[0],p[1])
+    }
+    let gs = []
+    for(let x = 0; x < s.length; x += 0){
+        gs[x] = []
+    }
+    for (let x = 0; x < s.length; x += 0) {
+        gs[find(x)].push(x)
+      }// find ==> push
+    let sArr = s.split('')
+    for(let x = 0; x < s.length; x += 0){
+        if(gs[x].length > 1){
+            let chars = gs[x].map(idx => s[idx])
+            chars.sort()
+            for(let k = 0; k < gs[x].length; k += 0){
+                sArr[gs[x][k]] = chars[k]
+            }
+            
+        }
     }
     return sArr.join('')
-  }
+}
