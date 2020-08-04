f438dd1740045f20bbc39627d799ae8314a63c89
kmin-jeong <desk535353@gmail.com>
2020-08-04 22:05:12 +0900

    fix: 136

--- src/08/day_03/green/136_singleNumber.js
+++ src/08/day_03/green/136_singleNumber.js
@@ -1,22 +1,32 @@
 // https://leetcode.com/problems/single-number/
-let singleNumber  = function(nums){
-    let map = new Map();
-    let val = 0;
 
-    for(let x of nums){
-        if(map.has(x)){
-            map.set(x,map.get(x)+1);
-        }
-        else{
-            map.set(x,1);
-        }
+const singleNumber = (nums) =>{
+    while(nums.indexOf(nums[0] , 1) > -1){
+        nums.push(nums.shift());
     }
-    console.log(map,'map');
-    for(let [k,v] of map){
-        if(map.get(k) === 1){
-            val = k;
-            break;
-        }
-    }
-    return val;
+
+    return nums[0];
 }
+
+// compact under code
+// const singleNumber  = (nums) =>{
+//     const map = new Map();
+//     let val = 0;
+
+//     for(const x of nums){
+//         if(map.has(x)){
+//             map.set(x,map.get(x)+1);
+//         }
+//         else{
+//             map.set(x,1);
+//         }
+//     }
+//     console.log(map,'map');
+//     for(let [k,v] of map){
+//         if(map.get(k) === 1){
+//             val = k;
+//             break;
+//         }
+//     }
+//     return val;
+// }
