cda77c705171d6932d3c59b2913403cbb0b23b0d
kmin-jeong <desk535353@gmail.com>
2020-08-04 11:08:36 +0900

    fix: fix 136_singleNumber errors

--- src/08/day_03/green/136_singleNumber.js
+++ src/08/day_03/green/136_singleNumber.js
@@ -1,4 +1,22 @@
 // https://leetcode.com/problems/single-number/
-var singleNumber = function(nums) {
-     return nums.reduce((res, n) => res ^ n, 0)
- };
+let singleNumber  = function(nums){
+    let map = new Map();
+    let val = 0;
+
+    for(let x of nums){
+        if(map.has(x)){
+            map.set(x,map.get(x)+1);
+        }
+        else{
+            map.set(x,1);
+        }
+    }
+    console.log(map,'map');
+    for(let [k,v] of map){
+        if(map.get(k) === 1){
+            val = k;
+            break;
+        }
+    }
+    return val;
+}
