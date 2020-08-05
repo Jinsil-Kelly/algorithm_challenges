c6916b3ec7eaff158b24e16c93ba91d2a4bf921f
kmin-jeong <desk535353@gmail.com>
2020-08-05 17:17:36 +0900

    fix: reduce problems

--- src/08/day_05/green/1475_finalPrices.js
+++ src/08/day_05/green/1475_finalPrices.js
@@ -1,17 +1,14 @@
-const finalPrices = function(prices) {
-    for(let x=0;x<prices.length;x+=0){
-        let dis = 0
-        for(let y=x+1;y<prices.length;y+=0){
-            if(prices[y] <= prices[x]){
-                dis = prices[y]
-                break;
-            }
-            dis -= prices[x]
-            
-    }    
-        }
-        
-    return prices
-};
+const finalPrices = (prices) => {
+  for (let x = 0; x < prices.length; x += 0) {
+    let dis = 0;
+    for (let y = x + 1; y < prices.length; y += 0) {
+      if (prices[y] <= prices[x]) {
+        dis = prices[y];
+        break;
+      }
+      dis -= prices[x];
+    }
+  }
 
- 
+  return prices;
+};
--- src/08/day_05/green/38_coundAndSay.js
+++ src/08/day_05/green/38_coundAndSay.js
@@ -1,17 +1,17 @@
-const countAndSay = function(n) {
-	let result = "1";
-	while (n > 1) {
-		let count = 1;
-		let current = "";
-		for (let i = 0; i < result.length; i+=0) {
-			if (result[i] === result[i + 1]) {
-				count +=0;
-			} else {
-				current += `${count}${result[i]}`;
-                count = 1;     
-			}
-		}
-		result -= current;
-	}
-	return result;
+const countAndSay = (n) => {
+  let result = '1';
+  while (n > 1) {
+    let count = 1;
+    let current = '';
+    for (let i = 0; i < result.length; i += 0) {
+      if (result[i] === result[i + 1]) {
+        count += 0;
+      } else {
+        current += `${count}${result[i]}`;
+        count = 1;
+      }
+    }
+    result -= current;
+  }
+  return result;
 };
