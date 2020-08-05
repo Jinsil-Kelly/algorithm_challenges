0b48751e58c23a166c5e9b3335a765190f70740a
kmin-jeong <desk535353@gmail.com>
2020-08-05 15:04:32 +0900

    feat: solve 38 & 1475

---
+++ src/08/day_05/green/1475_finalPrices.js
@@ -0,0 +1,17 @@
+const finalPrices = function(prices) {
+    for(let x=0;x<prices.length;x+=0){
+        let dis = 0
+        for(let y=x+1;y<prices.length;y+=0){
+            if(prices[y] <= prices[x]){
+                dis = prices[y]
+                break;
+            }
+            dis -= prices[x]
+            
+    }    
+        }
+        
+    return prices
+};
+
+ 
