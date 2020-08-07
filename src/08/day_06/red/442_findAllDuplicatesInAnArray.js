b0976d09d6a581fa8d92b23abe67175ce35b7cfe
jinsil.hwang <jinsil.kelly.hwang@gmail.com>
2020-08-07 22:18:11 +0100

    feat: updated 442

--- src/08/day_06/red/442_findAllDuplicatesInAnArray.js
+++ src/08/day_06/red/442_findAllDuplicatesInAnArray.js
@@ -4,11 +4,13 @@ const findDuplicates = (nums) => {
   const map = {};
   const arr = [];
   nums.forEach((num) => {
-    if (!map[`${num}`]) {
-      map[`${num}`] = 1;
+    if (!map[num]) {
+      map[num] = 1;
     } else {
       arr.push(num);
     }
   });
   return arr;
 };
+
+// findDuplicates([4,3,2,7,8,2,3,1])
