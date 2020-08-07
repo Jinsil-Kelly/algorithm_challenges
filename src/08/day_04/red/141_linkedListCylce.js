7e15f72988ad3534605927230f7213307935cd0a
jinsil.hwang <jinsil.kelly.hwang@gmail.com>
2020-08-07 22:04:29 +0100

    feat: updated 141

--- src/08/day_04/red/141_linkedListCylce.js
+++ src/08/day_04/red/141_linkedListCylce.js
@@ -1,13 +1,13 @@
 // https://leetcode.com/problems/linked-list-cycle/
 
 const hasCycle = (head) => {
-  if (head == null || head.next == null) return false;
+  if (head === null || head.next === null) return false;
 
   let ptr1 = head;
   let ptr2 = head.next;
 
   while (ptr1 !== ptr2) {
-    if (ptr2 == null || ptr2.next == null) return false;
+    if (ptr2 === null || ptr2.next === null) return false;
 
     ptr1 = ptr1.next;
     ptr2 = ptr2.next.next;
