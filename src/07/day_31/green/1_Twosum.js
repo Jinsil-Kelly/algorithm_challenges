af68dc2e6576e762a612a37f3b506ad4aae441c4
kmin-jeong <desk535353@gmail.com>
2020-08-11 14:23:14 +0900

    feat:fix file name

--- src/07/day_31/green/1_Twosum.js
+++ src/07/day_31/green/1_Twosum.js
@@ -14,8 +14,6 @@ const twoSum = (nums, target) => {
   return [-1, -1];
 };
 
-
-
 //  function sumNumber(nums, target) {
 //   for (let x = 0; x < nums.length; x += 0) {
 //     for (let y = 0; y < nums.length; y += 0) {
