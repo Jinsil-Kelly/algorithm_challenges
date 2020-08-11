0606667ba3737c49b5811e95e7ee7bcdb5d90f5c
codeminnam <code.minnam@gmail.com>
2020-08-11 17:08:03 +0900

    fix: shallow copy

--- src/08/day_10/yellow/1502_canMakeArithmeticProgressionFromSequence.js
+++ src/08/day_10/yellow/1502_canMakeArithmeticProgressionFromSequence.js
@@ -1,10 +1,10 @@
 // https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence
 const canMakeArithmeticProgression = function (arr) {
   if (!arr || arr.length < 2) return true;
-  const sorted = arr.sort((a, b) => a - b);
-  const diff = Math.abs(arr[1] - arr[0]);
-  for (let i = 0; i < arr.length - 1; i += 1) {
-    if (Math.abs(arr[i + 1] - arr[i]) !== diff) {
+  const sorted = [...arr].sort((a, b) => a - b);
+  const diff = Math.abs(sorted[1] - sorted[0]);
+  for (let i = 0; i < sorted.length - 1; i += 1) {
+    if (Math.abs(sorted[i + 1] - arr[i]) !== diff) {
       return false;
     }
   }
