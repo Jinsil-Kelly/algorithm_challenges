75dc624f9bac8f148bb52aa827187b01871c3281
jinsil.hwang <jinsil.kelly.hwang@gmail.com>
2020-08-12 23:14:04 +0100

    feat: added 2 & 8

--- src/08/day_12/red/8_myAtoi.js
+++ src/08/day_12/red/8_myAtoi.js
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/string-to-integer-atoi/
-const MIN = -2147483648;
-const MAX = 2147483647;
 
 const myAtoi = function (str) {
+  const MIN = -2147483648;
+  const MAX = 2147483647;
   return Math.max(Math.min(parseInt(str, 10) || 0, MAX), MIN);
 };
