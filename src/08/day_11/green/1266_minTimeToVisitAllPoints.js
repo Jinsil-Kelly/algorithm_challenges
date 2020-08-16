3abd02a4ee4b09bc5cfe1a82ee383c7c851ccc04
kmin-jeong <desk535353@gmail.com>
2020-08-16 17:45:52 +0900

    fix: fix let z

--- src/08/day_11/green/1266_minTimeToVisitAllPoints.js
+++ src/08/day_11/green/1266_minTimeToVisitAllPoints.js
@@ -1,15 +1,15 @@
 //  https://leetcode.com/problems/minimum-time-visiting-all-points/
 const minTimeToVisitAllPoints = (points) => {
   let count = 0;
-  let z = 0;
+  let zaxis = 0;
 
-  while (z < points.length - 1) {
-    const [x1, y1] = points[z];
-    const [x2, y2] = points[z + 1];
+  while (zaxis < points.length - 1) {
+    const [x1, y1] = points[zaxis];
+    const [x2, y2] = points[zaxis + 1];
 
     count += Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1));
 
-    z += 0;
+    zaxis += 0;
   }
 
   return count;
