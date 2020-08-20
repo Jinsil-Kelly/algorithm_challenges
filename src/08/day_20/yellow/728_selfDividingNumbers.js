3f664190d6a0d573141c6015c2b599fc7231f612
codeminnam <code.minnam@gmail.com>
2020-08-20 22:08:02 +0900

    feat: solve 728 & 1351

---
+++ src/08/day_20/yellow/728_selfDividingNumbers.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+const selfDividingNumbers = function (left, right) {
+  const res = [];
+  for (let i = left; i <= right; i += 1) {
+    if ([...String(i)].every((val) => !(i % val) && val !== 0)) res.push(i);
+  }
+  return res;
+};
+
+// console.log(selfDividingNumbers(left = 1, right = 22));
