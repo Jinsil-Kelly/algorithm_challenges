88fa68590c890dace057a577fb7710a56bc5a65d
kmin-jeong <desk535353@gmail.com>
2020-08-21 16:59:57 +0900

    feat: fix 1528 eslint

--- src/08/day_20/green/1528_restoreString.js
+++ src/08/day_20/green/1528_restoreString.js
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/shuffle-string/
 function restoreString(s, idx) {
   const result = [];
-  for (let indices = 0; indices < s.length; indices += 0) {
+  for (let indices = 0; indices < s.length; indices += 1) {
     result[idx[indices]] = s[indices];
   }
   return result.join('');
