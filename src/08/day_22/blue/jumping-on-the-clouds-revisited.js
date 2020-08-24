478f1285b30c88dfe1c8035d79d4fed6fb6ea86c
Mina Lee <minaleedev@gmail.com>
2020-08-24 16:04:23 +0900

    feat: refactor jumpingOnClouds

--- src/08/day_22/blue/jumping-on-the-clouds-revisited.js
+++ src/08/day_22/blue/jumping-on-the-clouds-revisited.js
@@ -1,14 +1,13 @@
 // https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
 
 const jumpingOnClouds = (c, k) => {
-  const n = c.length;
-  let energy = 100;
-  for (let i = 0; ; ) {
-    i += k;
-    if (i >= n) i %= n;
-    energy -= !c[i] ? 1 : 3;
+  let index = 0;
+  let e = 100;
 
-    if (!i) break;
-  }
-  return energy;
+  do {
+    index = (index + k) % c.length;
+    e -= c[index] ? 3 : 1;
+  } while (index);
+
+  return e;
 };
